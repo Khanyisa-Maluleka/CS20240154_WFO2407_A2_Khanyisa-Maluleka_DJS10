c57e287000949a2f0f0a3709935c282806429d28
Khanyisa Maluleka <khanyisamaluleka20@gmail.com>
2024-11-22 00:52:18 +0200

    Added the post ids

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ export default function App() {
         <h1>Posts</h1>
           {posts.map(post => (
             <div key={post.id}>
-              <h2>{post.title}</h2>
+              <h2>{post.id}. {post.title}</h2>
               <p>{post.body}</p>
             </div>
           ))}
