5aae52565cc6e87091e044794efed8fbda563ea6
Khanyisa Maluleka <khanyisamaluleka20@gmail.com>
2024-11-21 23:17:49 +0200

    Fetched my data

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ function App() {
     useEffect (() => {
 
       const fetchPost = async () => {
-        
+
+        const data = await fetch ('https://jsonplaceholder.typicode.com/posts');
       }
     }, [])
 }
